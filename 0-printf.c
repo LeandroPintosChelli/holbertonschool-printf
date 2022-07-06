e77d4d2ea709beea2dfd2f43d53af61daf048388
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-06 09:47:43 -0700

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -3,8 +3,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-
 int _printf(const char *format, ...)
+{
+	va_list all;
+	unsigned int i, n;
+	char *s;
+
+	va_start(all, i);
+
+	for (n = 0; n < i; n++)
+	{
+		s = va_arg(all, char *);
+
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (format != NULL && i < n -1)
+			printf("%s", format);
+	}
+	va_end(all);
+	printf("\n");
+}
+
+/**int _printf(const char *format, ...)
 {
 	va_list all;
 	int i = 0;
@@ -31,4 +53,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(all);
 	printf("\n");
-}
+}*/
