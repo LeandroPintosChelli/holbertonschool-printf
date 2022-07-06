162d8caaa90c64278f352951b72dc9779b612543
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-06 09:25:28 -0700

    0-printf.c

---
+++ 0-printf.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list all;
+	int leng, i = 0;
+	char *f;
+
+	va_start(all, format);
+	if (format)
+	{
+		leng = strlen(format);
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c';
+					printf("%c", va_arg(all, char));
+					break;
+				case 's';
+					printf("%s", va_arg(all, char));
+					break;
+				case '%';
+					printf("%%", va_arg(all, char));
+					break;
+			}
+			i++;
+		}
+	}
+	va_end(all);
+	printf('\n');
+}
