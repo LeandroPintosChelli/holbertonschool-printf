6ae874060f561eed71cd9bbecbd23c8049ee1479
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-06 13:49:54 -0700

    main.h

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-void print_nuber(int n);
-
+#include <stdarg.h>
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list all);
+} op;
+int _printf(const char *format, ...);
+int _putchar(char c);
 #endif
