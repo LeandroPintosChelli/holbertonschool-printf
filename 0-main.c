5be3b6c99195d2dbcf64518a950bd11aef4cbe70
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-07 05:56:01 -0700

    printf

--- 0-main.c
+++ 0-main.c
@@ -1,7 +1,9 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     printf("Hello");
-    return 0;
+    return (0);
 }
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 typedef struct op
 {
 	char op;
