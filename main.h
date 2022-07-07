1ee4ad732c958230ad8186862277f7af468630ae
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-07 05:18:22 -0700

    git

--- main.h
+++ main.h
@@ -3,9 +3,11 @@
 #include <stdarg.h>
 typedef struct op
 {
-	char *op;
+	char op;
 	int (*f)(va_list all);
-} op;
+} op_t;
+int pr_str(va_list);
+int pr_char(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
 #endif
