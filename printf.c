d5ff0ce2d28d5e76a0e066656caedee203ed44fa
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-08 07:41:40 -0700

    .

--- printf.c
+++ printf.c
@@ -54,7 +54,7 @@ int _while(va_list all, char form)
 	};
 	int j = 0;
 
-	while (j < 2) /**chequea las posc de la estr y recorre*/
+	while (j < 4) /**chequea las posc de la estr y recorre*/
 	{
 		if (*ops[j].op == form)
 		{
